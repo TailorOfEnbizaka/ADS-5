099910d5b02a267bc565ceff43c7cb3537f315c8
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-04-09 13:22:04 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -1,10 +1,28 @@
 // Copyright 2021 NNTU-CS
 #ifndef INCLUDE_TSTACK_H_
 #define INCLUDE_TSTACK_H_
+#include <string>
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+T data[size];
+int top;
+public:
+TStack() {
+  top(0);
+}
+void posh(const T& item) {
+  if(top<=size-1)
+    data[top++]=item;
+  else
+    throw std::string("Full!");
+}
+T pop() {
+  if(top>0)
+    return data[--top];
+  throw std::string("Empty");
+}
 };
 
 #endif  // INCLUDE_TSTACK_H_
