c34f9a353e1d061c30cfd384c1cfd927a5fcbaae
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-04-15 23:08:13 +0300

    Update tstack.h

--- include/tstack.h
+++ include/tstack.h
@@ -4,33 +4,32 @@
 #include <string>
 template<typename T, int size>
 class TStack {
-	// добавьте код стека
+// добавьте код стека
 private:
-	T data[100];
-	int top;
+    T data[100];
+    int top;
 public:
-	TStack() : top(-1) {}
-	void posh(T value) {
-		if (top != size - 1) {
-			data[++top] = value;
-		}
-		else {
-			throw std::string("Full");
-		}
-	}
-	bool empty() {
-		return top == -1;
-	}
-	T got() const {
-		return data[top];
-	}
-	T pop() {
-		if (empty()) {
-			throw std::string("Empty");
-		}
-		else {
-			return data[top--];
-		}
-	}
+    TStack() : top(-1) {}
+    void posh(T value) {
+        if (top != size - 1) {
+            data[++top] = value;
+        } else {
+            throw std::string("Full");
+        }
+    }
+    bool empty() {
+        return top == -1;
+    }
+    T got() const {
+        return data[top];
+    }
+    T pop() {
+        if (empty()) {
+            throw std::string("Empty");
+        }
+        else {
+        return data[top--];
+        }
+    }
 };
 #endif  // INCLUDE_TSTACK_H_
