3160ed81a4b6d3ef82c5bd73ca1f6e754a35bf90
TailorOfEnbizaka <160338576+TailorOfEnbizaka@users.noreply.github.com>
2024-04-15 23:09:43 +0300

    Update tests.cpp

--- test/tests.cpp
+++ test/tests.cpp
@@ -10,14 +10,12 @@ TEST(lab3, test1_1) {
     std::string in = "(2+2)*2";
     std::string exp = "2 2 + 2 *";
     std::string out = infx2pstfx(in);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_TRUE(out == exp);
 }
 
 TEST(lab3, test1_2) {
     std::string exp = "2 2 + 2 *";
     int out = eval(exp);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_EQ(out, 8);
 }
 
@@ -25,14 +23,12 @@ TEST(lab3, test2_1) {
     std::string in = "(2-1)*(6+2)";
     std::string exp = "2 1 - 6 2 + *";
     std::string out = infx2pstfx(in);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_TRUE(out == exp);
 }
 
 TEST(lab3, test2_2) {
     std::string exp = "2 1 - 6 2 + *";
     int out = eval(exp);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_EQ(out, 8);
 }
 
@@ -40,14 +36,12 @@ TEST(lab3, test3_1) {
     std::string in = "(5+2)/6-(4+3)*5";
     std::string exp = "5 2 + 6 / 4 3 + 5 * -";
     std::string out = infx2pstfx(in);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_TRUE(out == exp);
 }
 
 TEST(lab3, test3_2) {
     std::string exp = "5 2 + 6 / 4 3 + 5 * -";
     int out = eval(exp);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_EQ(out, -34);
 }
 
@@ -55,13 +49,11 @@ TEST(lab3, test4_1) {
     std::string in = "8*(3+7)/2-(3+7)*9";
     std::string exp = "8 3 7 + * 2 / 3 7 + 9 * -";
     std::string out = infx2pstfx(in);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_TRUE(out == exp);
 }
 
 TEST(lab3, test4_2) {
     std::string exp = "8 3 7 + * 2 / 3 7 + 9 * -";
     int out = eval(exp);
-    std::cout<<"out= "<<out<<std::endl;
     EXPECT_EQ(-50, out);
 }
